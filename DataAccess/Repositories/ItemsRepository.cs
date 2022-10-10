b1cab1bfd93aa363012f8b52877c514c841e991a
Ryan Attard <ryanattard@gmail.com>
2022-10-10 08:28:33 +0200

    first commit with basic architecutre

---
+++ DataAccess/Repositories/ItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    class ItemsRepository
+    {
+        public IQueryable<Item> GetItems()
+        { }
+
+        public void AddItem() 
+        { }
+    }
+}
