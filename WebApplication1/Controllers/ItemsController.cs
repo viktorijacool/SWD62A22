3d0c7da265c2e1a9d36617a8f659da06e3a0cea2
Viktorija K <viktoria.kuluk@inbox.lv>
2022-11-01 13:49:52 +0200

    Photo insertion

--- WebApplication1/Controllers/ItemsController.cs
+++ WebApplication1/Controllers/ItemsController.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLogic.Services;
 using BusinessLogic.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +17,12 @@ namespace WebApplication1.Controllers
     public class ItemsController : Controller
     {
         private ItemsServices itemsServices;
+        private IWebHostEnvironment host;
 
-        public ItemsController(ItemsServices _itemsServices)
+        public ItemsController(ItemsServices _itemsServices, IWebHostEnvironment _host)
         {
             itemsServices = _itemsServices;
+            host = _host;
         }
 
 
@@ -31,10 +36,35 @@ namespace WebApplication1.Controllers
 
         //a method to handle the submission of the form 
         [HttpPost]
-        public IActionResult Create(CreateItemViewModel data)
+        public IActionResult Create(CreateItemViewModel data, IFormFile file)
         {   //.....
             try
             {
+                if (file != null)
+                {
+                    //1. change filename
+                    string uniqueFilename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(file.FileName);
+
+                    //2. the absolute path of the folder where the image is going...
+                    //e.g. D:\MCAST\Enterprise Proramming\EnterpriseProgrammingSolution\WebApplication1\wwwroot\Images\
+
+                    string absolutePath = host.WebRootPath;
+
+                    //3. saving file
+                    using (System.IO.FileStream fsOut = new System.IO.FileStream(absolutePath + "\\Images\\" + uniqueFilename, System.IO.FileMode.CreateNew))
+                    {
+                        file.CopyTo(fsOut);
+                    }
+
+                    //4. save the path to the image in the database
+                    //http://localhost:xxxx/Images/filename.jpg
+                    data.PhotoPath = "/Images/" + uniqueFilename;
+                }
+
+
+
+
+
                 itemsServices.AddItem(data);    //to test
                 //dynamic object - it builds the declared properties on-the-fly i.e. the moment you declare the property
                 //"Message" - it builds in realtime in memory
